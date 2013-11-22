06d6341080f0158c4ae5d3b1989a0f607dccecdb
Paul Campbell <paul.campbell@stonybrook.edu>
2013-11-21 22:09:43 -0500

    Added sample and libhook.so to the git ignore. Added a header file for hook.c. Added a hook to the free function call.

--- ldpreload/hook.c
+++ ldpreload/hook.c
@@ -2,16 +2,53 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <dlfcn.h> /* dlsym() */
+#include "hook.h"
 
-void *malloc(size_t size) {
-	static void *(*my_malloc)(size_t) = NULL;
-	printf("inside shared object..\n");
-	if(!my_malloc) {
-		my_malloc = dlsym(RTLD_NEXT, "malloc");
+/**
+ * Stores the address of malloc into the function pointer.
+ */
+static void mallocInit(void) {
+    original_malloc = dlsym(RTLD_NEXT, "malloc");
+    if (original_malloc == NULL) {
+        fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
+    }
+}
+
+/**
+ * Stores the address of free into the function pointer.
+ */
+static void freeInit(void) {
+	original_free = dlsym(RTLD_NEXT, "free");
+	if(original_free == NULL) {
+		fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
 	}
-	void *p = my_malloc(size);
+}
+
+/**
+ * Custom wrapper for malloc. 
+ * Will track all allocated pointers and log lost memory.
+ * @param size Amount of bytes to allocate.
+ * @return Returns memory allocated by malloc if possible, else NULL.
+ */
+void *malloc(size_t size) {
+	if(original_malloc == NULL) {
+		mallocInit();
+	}
+	void *p = original_malloc(size);
 	printf("malloc(%zd) = %p\n", size, p);
-	lcheck();
-	printf("returning from shared object..\n");
 	return p;
+}
+
+/**
+ * Custom wrapper for free.
+ * Can detect free on null, unaligned free, free of an
+ * already freed pointer (double free), and free of stack memory.
+ * @param ptr The pointer to be deallocated.
+ */
+void free(void *ptr) {
+	if(original_free == NULL) {
+		freeInit();
+	}
+	original_free(ptr);
+	printf("free(%p)", ptr);
 }
---
+++ ldpreload/hook.h
@@ -0,0 +1,13 @@
+#ifndef HOOK_H
+#define HOOK_H
+/* Location to store the original malloc function pointer */
+static void* (*original_malloc)(size_t) = NULL;
+/* Location to store the original free function pointer */
+static void* (*original_free)(void*) = NULL;
+/* Function for initializing and storing the function pointer of the original malloc */
+static void mallocInit(void);
+/* Function for initializing and storing the function pointer of the original free */
+static void freeInit(void);
+/* Custom wrapper function for malloc */
+void *malloc(size_t size);
+#endif
