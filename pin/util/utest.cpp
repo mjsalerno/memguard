7474ea60f4cc5703a30995a48c6e068136fca50f
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-14 16:22:31 -0500

    Added tests to utest.cpp for whitelist.cpp

--- pin/util/utest.cpp
+++ pin/util/utest.cpp
@@ -2,10 +2,22 @@
 #include <cassert>
 #include <iostream>
 #include "memoryalloc.h"
+#include "whitelist.h"
 
 using namespace std;
 
+void memoryAllocTests();
+void whiteListTests();
+
 int main(int argc, char *argv[]) {
+	/* Test nodes */
+	memoryAllocTests();
+	/* if node tests pass, test the list */
+	whiteListTests();
+	return EXIT_SUCCESS;
+}
+
+void memoryAllocTests() {
 	// Allocate Some pointers
 	char *string = (char*) malloc(56);
 	char *s2 = (char*) malloc(102);
@@ -29,5 +41,58 @@ int main(int argc, char *argv[]) {
 	// Free allocated memory
 	free(string);
 	free(s2);
-	return EXIT_SUCCESS;
+}
+
+void whiteListTests() {
+	WhiteList wlist;
+	int size1 = 1337;
+	char *s1 = (char*) malloc(size1);
+	// Check the initial size;
+	assert(wlist.size() == 0);
+	// Create a new node
+	wlist.add(s1, size1);
+	// Check the size
+	assert(wlist.size() == 1);
+	// Remove the node
+	bool success = wlist.remove(0);
+	assert(success == true);
+	assert(wlist.size() == 0);
+	assert(wlist.isEmpty());
+	// Add the node back
+	MemoryAlloc ma1(s1, size);
+	wlist.add(ma1);
+	// Check the containsAddress function
+	int index = wlist.containsAddress(ma1.getAddress());
+	assert(index >= 0);
+	index = wlist.containsAddress(s1);
+	assert(index >= 0);
+	assert(index == 0); // Only item inserted so it must be at index 0 right?
+	index = wlist.containsAddress(NULL);
+	assert(index == ERR_NOT_FOUND);
+	index = wlist.containsAddress((void*)((char*)ma1.getAddress() + 1));
+	assert(index == ERR_MID_CHUNK);
+	// Check the get function
+	index = wlist.containsAddress(s1);
+	MemoryAlloc g = wlist.get(index); 
+	assert(g.getAddress() == ma1.getAddress());
+	assert(g.getSize() == ma1.getSize());
+	// Check removeMatching(void* address)
+	bool removed = wlist.removeMatching(s1);
+	assert(removed);
+	removed = wlist.removeMatching(NULL);
+	assert(!removed);
+	// Check removeMatching(MemoryAlloc &alloc)
+	wlist.add(ma1);
+	removed = wlist.removeMatching(ma1);
+	assert(removed);
+	removed = wlist.removeMatching(ma1);
+	assert(!removed);
+	// Check clear()
+	assert(wlist.isEmpty());
+	wlist.add(ma1);
+	assert(!wlist.isEmpty());
+	wlist.clear();
+	assert(wlist.isEmpty());
+	// Free allocated memory
+	free(s1);
 }
