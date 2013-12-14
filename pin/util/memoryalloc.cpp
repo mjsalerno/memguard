397bc4c12e6d4455aa94724a14e7b34d88e28de8
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-14 14:12:51 -0500

    Added a main file for testing.

---
+++ pin/util/memoryalloc.cpp
@@ -0,0 +1,31 @@
+#include "memoryalloc.h"
+
+MemoryAlloc::MemoryAlloc() {
+	this.address = NULL;
+	this.size = 0;
+}
+
+MemoryAlloc::MemoryAlloc(void *address, unsigned int size) {
+	this.address = address;
+	this.size = size;
+}
+
+void* MemoryAlloc::getAddress() {
+	return this.address;
+}
+
+void MemoryAlloc::setAddress(void *address) {
+	this.address = address;
+}
+
+unsigned int MemoryAlloc::getSize() {
+	return this.size;
+}
+
+void MemoryAlloc::setSize(unsigned int size) {
+	this.size = size;
+}
+
+void MemoryAlloc::print() {
+	cout << "[" << this.address << " ~ " << this.size << "]" << endl;
+}
---
+++ pin/util/utest.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include "memoryalloc.h"
+
+int main(int argc, char *argv[]) {
+	MemoryAlloc ma;
+
+	ma.print();
+
+	return EXIT_SUCCESS;
+}
