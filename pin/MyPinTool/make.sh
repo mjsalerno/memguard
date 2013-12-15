785d895d514f7b9f74324697847ce30821e53e3e
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-14 21:08:53 -0500

    Added a clean flag to make.sh

--- pin/MyPinTool/make.sh
+++ pin/MyPinTool/make.sh
@@ -1,3 +1,10 @@
-make PIN_ROOT=pin
-gcc test.c -o test
-./pin/pin -t obj-intel64/mallocmem.so -- ./test
+#!/usr/bin/env bash
+
+if test "$1" = "clean"  ; then
+	make PIN_ROOT=pin clean
+else
+	# Just ignore the flag and do the normal build 
+	make PIN_ROOT=pin
+	gcc test.c -o test
+	./pin/pin -t obj-intel64/mallocmem.so -- ./test	
+fi
