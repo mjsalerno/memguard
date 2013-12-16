42461f224597f8153f21618fb7d1bf16b3852530
Michael Salerno <michael.j.salerno@stonybrook.edu>
2013-12-15 19:48:26 -0500

    removed things from the stach read/write

--- pin/MyPinTool/test.c
+++ pin/MyPinTool/test.c
@@ -4,7 +4,8 @@
 int main(int argc, char const *argv[]) {
 	int j = 0;
 	char *ptr = NULL;        
-	char* cp1 = malloc(16);    
+	char* cp1 = malloc(16);
+	
     // printf("this will be heap\n");
     *(cp1+9) = 'q';
     // printf("that was heap\n");	
