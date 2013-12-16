ce95f740f5595241e4a5b47055dab379c2d8c1e7
Michael Salerno <michael.j.salerno@stonybrook.edu>
2013-12-15 23:52:21 -0500

    let the make script work for 32 bit comps

--- pin/MyPinTool/make.sh
+++ pin/MyPinTool/make.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -d "obj-intel64" ] ; then
+	OBJDIR="obj-intel64"
+else
+	OBJDIR="obj-ia32"
+fi
+
+
 if test "$1" = "clean"  ; then
 	make PIN_ROOT=pin clean
 else
 	# Just ignore the flag and do the normal build 
 	make PIN_ROOT=pin
 	gcc test.c -o test
-	./pin/pin -t obj-intel64/mallocmem.so -- ./test	
+	./pin/pin -t "$OBJDIR"/mallocmem.so -- ./test	
 fi
