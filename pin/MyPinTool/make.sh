7351ad9e5a146150d79733125ea70090885b4615
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-16 21:27:27 -0500

    Fixed logic for 32 and 64 bit directory.

--- pin/MyPinTool/make.sh
+++ pin/MyPinTool/make.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -d "obj-intel64" ] ; then
-	OBJDIR="obj-intel64"
-else
-	OBJDIR="obj-ia32"
-fi
-
-
 if test "$1" = "clean"  ; then
 	make PIN_ROOT=pin clean
 	rm -f test 
@@ -14,5 +7,12 @@ else
 	# Just ignore the flag and do the normal build 
 	make PIN_ROOT=pin
 	gcc test.c -o test
+	# Check for 32 or 64 bit system 
+	if [ -d "obj-intel64" ] ; then
+		OBJDIR="obj-intel64"
+	else
+		OBJDIR="obj-ia32"
+	fi
+	# Run Pin
 	./pin/pin -t "$OBJDIR"/mallocmem.so -- ./test	
 fi
