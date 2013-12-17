775c3d1d78330e01cbc60f36f49050f065f7be2a
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-16 21:31:41 -0500

    Added checks to make.sh for successful compilation.

--- pin/MyPinTool/make.sh
+++ pin/MyPinTool/make.sh
@@ -6,7 +6,18 @@ if test "$1" = "clean"  ; then
 else
 	# Just ignore the flag and do the normal build 
 	make PIN_ROOT=pin
+	# Check to see if pin was successful
+	if [ "$?" = "1" ]
+		echo "pintool make failed."
+		exit 1
+	fi
+	# compile the test program
 	gcc test.c -o test
+	# Check to see if the test program compiled successfully
+	if [ "$?" = "1" ]
+		echo "test program failed to compile."
+		exit 1
+	fi
 	# Check for 32 or 64 bit system 
 	if [ -d "obj-intel64" ] ; then
 		OBJDIR="obj-intel64"
