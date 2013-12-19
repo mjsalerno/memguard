65b8d5ac53680223c4e8b69bed27c66323d8edef
Paul Campbell <paul.campbell@stonybrook.edu>
2013-12-18 22:09:32 -0500

    Added accessor for freeNotFoundCount.

--- pin/MyPinTool/stats.h
+++ pin/MyPinTool/stats.h
@@ -6,14 +6,21 @@ class Stats {
 private:
 	unsigned int mallocCount;
 	unsigned int freeCount;
+	
 	unsigned int invalidReadCount;
 	unsigned int invalidWriteCount;
+	
 	unsigned int invalidFreeCount;
 	unsigned int midFreeChunkCount;
 	unsigned int freeNullCount;
+	
+	unsigned int freeNotFoundCount;
+
+
 	unsigned int fenceHitCount;
 	unsigned int fenceOverflowHitCount;
 	unsigned int fenceUnderflowHitCount;
+	
 	unsigned int invalidReturnCount;
 public:
 	Stats();
@@ -24,6 +31,7 @@ public:
 	unsigned int getFenceHitCount();
 	unsigned int getInvalidFreeCount();
 	unsigned int getMidFreeChunkCount();
+	unsigned int getFreeNotFoundCount();
 	unsigned int getFreeNullCount();
 	unsigned int getInvalidReturnCount();
 	unsigned int getFenceOverflowHitCount();
@@ -36,6 +44,7 @@ public:
 	void setFenceHitCount(unsigned int count);
 	void setInvalidFreeCount(unsigned int count);
 	void setMidFreeChunkCount(unsigned int count);
+	void setFreeNotFoundCount(unsigned int count);
 	void setFreeNullCount(unsigned int count);
 	void setInvalidReturnCount(unsigned int count);
 	void setFenceOverflowHitCount(unsigned int count);
@@ -48,6 +57,7 @@ public:
 	void incFenceHitCount();
 	void incInvalidFreeCount();
 	void incMidFreeChunkCount();
+	void incFreeNotFoundCount();
 	void incFreeNullCount();
 	void incInvalidReturnCount();
 	void incFenceOverflowHitCount();
